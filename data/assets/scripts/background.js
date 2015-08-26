b8d7155a64aa4ac3e8ba75234fa1aa718592f5d6
CodyL19 <codylarg@gmail.com>
2015-08-26 14:52:12 -0500

    Panel resize ability added, and ability to connect to the add-on
    Next will be to grab info for connected user

--- data/assets/scripts/background.js
+++ data/assets/scripts/background.js
@@ -1,10 +1,29 @@
-var panelWidth  = $('body').width();
-var panelHeight = $('body').height();
+/*
+ * Page use: All messages will be sent from here, and will process recieved messages.
+ *
+ * Notes:
+ * Will Run in background
+ * Some things done to panel using page may need to be redone each time panel is opened
+ */
 
-addon.port.on("shown", function() {
-    addon.port.emit('alert', "is connected");
+///////////////////////    Run the following only when the panel is open.    ///////////////////////
+
+addon.port.on("foo", function() {
+      // Connect page height : addon.port.emit('height', 202);
+    // Connected page height : addon.port.emit('height', 546);
+
+    if(!localStorage.getItem('connected')){ // Is a connection saved?
+      addon.port.emit('height', 202);
+
+      // URL Structure for creating a new connection: https://www.twii.me/mpb/connect/?apikey={PUBLIC_KEY}&apicode={TEMP_CODE}
+      $('#connect button').click(function(){ addon.port.emit("firstConnection"); }); // Create a new connection
+
+    } else { addon.port.emit('height', 546); }
+
 });
 
-addon.port.on("alert", function(message){
-    console.log("Connected: "+message);
+///////////////    The following is to process messages regardless of panel state    ///////////////
+
+addon.port.on("firstConnection", function(){
+  // Proccess the connection info
 });
